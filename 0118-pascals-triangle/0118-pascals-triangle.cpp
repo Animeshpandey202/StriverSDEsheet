aae73ca47e320808c5ba94438fffbe22d6765b4c
Animesh Pandey <98416957+Animeshpandey202@users.noreply.github.com>
2023-10-02 13:35:21 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (6.92%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -2,22 +2,29 @@ class Solution {
 public:
     vector<vector<int>> generate(int n) {
         
-     vector<vector<int>>ans;
-     ans.push_back({1});
-        
-     for(int i=1;i<n;i++){
-         vector<int>v;
-        for(int j=0;j<=i;j++){
-         
-            if(j==0 || j==i){
-                v.push_back(1);
-            }
-            else{
-                v.push_back(ans[i-1][j-1] +ans[i-1][j]);
+        //time for print all elements of n rows=O(n^2) space=O(n^2)
+        vector<vector<int>> r(n);
+        for(int i=0;i<n;i++){
+            
+            r[i].resize(i+1);
+            r[i][0]=r[i][i]=1;
+            
+            for(int j=1;j<i;j++){
+                r[i][j]= r[i-1][j-1]+r[i-1][j];
             }
         }
-         ans.push_back(v);
-     }
-        return ans;
+        return r;
+        
+        //to print an element of pascal t nth row rth column= formula (n-1Cr-1 )t=O(n) s=O(n)
+//          long long res = 1;
+
+//         // calculating nCr:
+//         for (int i = 0; i < r; i++) {
+//             res = res * (n - i);
+//             res = res / (i + 1);
+//         }
+//         return (int)(res);
+
+    
     }
 };
