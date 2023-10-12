ac006de7f35f932f3329708358c3cc5324f5111e
Animesh Pandey <98416957+Animeshpandey202@users.noreply.github.com>
2023-10-12 22:39:33 +0530

    Time: 12 ms (62.08%), Space: 11.2 MB (28.27%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,43 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-// brute force time=O(n^2)        
-//         int n=nums.size();
-//         for(int i=0;i<n;i++){
-//             for(int j=i+1;j<n;j++){
-//                 if(nums[i]+nums[j]==target)return{i,j};
-                
-//             }
-//         }
-//         return{};
-
-        
-    
-//Optimal approach time=O(n) space=O(n)   
-    int n=nums.size();
-    unordered_map<int,int> m;
-    vector <int> ans;
-    
-    
-    for(int i=0;i<n;i++){
-        
-        if(m.find(target-nums[i])!=m.end()){
+        unordered_map<int,int>mp;
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++){
             
-            ans.push_back(m[target-nums[i]]);
-            ans.push_back(i);
-            return ans;
+            if(mp.find(target-nums[i])!=mp.end()){
+                ans.push_back(i);
+                ans.push_back(mp[target-nums[i]]);
+                return ans;
+            }
+            mp[nums[i]]=i;
+            
         }
-        m[nums[i]]=i;
-    }
         return {};
-        
-        
-        
-        
-        
-        
-        
-        
     }
 };
-
