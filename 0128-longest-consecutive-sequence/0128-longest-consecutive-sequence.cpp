bc754dc725014e094e98e9b157e675fdf7e754bc
Animesh Pandey <98416957+Animeshpandey202@users.noreply.github.com>
2023-10-23 00:42:52 +0530

    Time: 72 ms (83.09%), Space: 45.9 MB (81.06%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,70 +1,36 @@
-
-class Solution
-{
+class Solution {
 public:
-    int longestConsecutive(vector<int> &a){
-        unordered_set<int>st;
+    int longestConsecutive(vector<int>& nums) {
         
-        for(auto it:a){
+        if(nums.size()==0 || nums.size()==1)return nums.size();
+        sort(nums.begin(),nums.end());
+        
+        int maxi=1;
+        int count=1;
+        int prev=nums[0];
+        
+        //c=1   maxi=0
+        
+        //1  2  3  4 100 200
+        //         p   i   
+        
+        for(int i=1;i<nums.size();i++){
             
-            st.insert(it);
-        }
-        
-        int count=0;
-        int ans=0;
-        
-        for(auto i:st){
-            
-            if (st.find(i-1) == st.end()) { // isse kam element nahi hai
+            if(nums[i]==prev+1){
+                count++;        
+                prev=nums[i];
+              maxi=max(maxi,count);
                 
-                count=1;
-                while(st.find(i+1)!=st.end()){
-                    
-                    i=i+1;
-                    count++;
-                }
-            
             }
-            ans=max(ans,count);
-            
-            
-            
+            else if(nums[i]==prev){
+                continue;
+            }
+            else{
+                prev=nums[i];
+                  maxi=max(maxi,count);
+                count=1;
+            }
         }
-        return ans;
-        
-        
-        
-        
-  
+        return maxi;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
